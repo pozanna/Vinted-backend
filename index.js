5371e36cdc754e1491911e818a3b9ee0eabe45bf
Anna Pozdnikova <annapozdnikova@annas-macbook-pro.home>
2023-04-16 21:37:12 +0200

    DELETE comment of mongoose connect in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI); // ("mongodb://localhost/vinted");
+mongoose.connect(process.env.MONGODB_URI);
 
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
